46a89ce912214be8a091ee0a1157da98c3fac18d
0TheSpy <dima228@protonmail.ch>
2023-09-07 02:20:55 +0300

    Update dllmain.cpp

--- dllmain.cpp
+++ dllmain.cpp
@@ -3,13 +3,13 @@
 #define DEBUG
 //#define HWID
 //#define TIMEDACCESS
-
+ 
 #ifdef HWID
 #define HWIDSTRING if (strcmp(XorStr("{be5a05e9-f9bd-11ea-9a43-806e6f6e6963}"), hwProfileInfo.szHwProfileGuid)) 
 #endif 
 
 #include <Windows.h>
-#include <iostream>
+#include <iostream> 
 
 #include <dbg.h>
 #include <inetmessage.h>
@@ -70,6 +70,8 @@ DWORD dwPrepareSteamConnectResponse;
 typedef bool(__thiscall* PrepareSteamConnectResponseFn)(void*, int, const char*, uint64, bool, const netadr_t&, bf_write&);
 bool __fastcall Hooked_PrepareSteamConnectResponse(DWORD* ecx, void* edx, int keySize, const char* encryptionKey, uint64 unGSSteamID, bool bGSSecure, const netadr_t& adr, bf_write& msg)
 {
+	printfdbg("Hooked_PrepareSteamConnectResponse called\n");
+
 	static PrepareSteamConnectResponseFn PrepareSteamConnectResponse = (PrepareSteamConnectResponseFn)dwPrepareSteamConnectResponse;
 
 	srand(time(NULL));
@@ -151,7 +153,7 @@ public:
 	CNetMessage() {
 		m_bReliable = true;
 		m_NetChannel = NULL;
-	}
+	} 
 
 	virtual ~CNetMessage() {};
 
@@ -163,7 +165,7 @@ public:
 	virtual void    SetNetChannel(INetChannel* netchan) { m_NetChannel = netchan; }
 	virtual bool	Process() { Assert(0); return false; };	// no handler set
 
-protected:
+//protected:
 	bool				m_bReliable;	// true if message should be send reliable
 	INetChannel* m_NetChannel;	// netchannel this message is from/for
 }; 
@@ -182,7 +184,7 @@ protected:
 	bool Process() { return m_pMessageHandler->Process##name( this ); }\
 
 #define net_SetConVar	5			// sends one/multiple convar settings
-
+  
 class NET_SetConVar : public CNetMessage
 {
 	DECLARE_NET_MESSAGE(SetConVar);
@@ -218,7 +220,7 @@ void* GetInterface(const char* dllname, const char* interfacename)
 	void* ointerface = CreateInterface(interfacename, &returnCode);
 	printfdbg("Interface %s/%s = %x\n", dllname, interfacename, ointerface);
 	return ointerface;
-}
+} 
 
 DWORD dwBuildConVarUpdateMessage;
 typedef void(__cdecl* BuildConVarUpdateMessageFn)(NET_SetConVar*, int, bool);
@@ -226,6 +228,8 @@ typedef void(__cdecl* BuildConVarUpdateMessageFn)(NET_SetConVar*, int, bool);
 //https://github.com/VSES/SourceEngine2007/blob/master/src_main/engine/host.cpp
 void Hooked_BuildConVarUpdateMessage(NET_SetConVar* cvarMsg, int flags, bool nonDefault) 
 {
+	printfdbg("Hooked_BuildConVarUpdateMessage called\n");
+
 	static BuildConVarUpdateMessageFn BuildConVarUpdateMessage = (BuildConVarUpdateMessageFn)dwBuildConVarUpdateMessage;
 	   
 	BuildConVarUpdateMessage(cvarMsg, flags, nonDefault);
@@ -236,30 +240,47 @@ void Hooked_BuildConVarUpdateMessage(NET_SetConVar* cvarMsg, int flags, bool non
 	strncpy(acvar.value, XorStr("spy"), MAX_OSPATH);
 	cvarMsg->m_ConVars.AddToTail(acvar);
 	
-	strncpy(acvar.name, XorStr("name"), MAX_OSPATH);
-	strncpy(acvar.value, XorStr("koronavirus"), MAX_OSPATH);
-	cvarMsg->m_ConVars.AddToTail(acvar);
-
+	//strncpy(acvar.name, XorStr("name"), MAX_OSPATH);
+	//strncpy(acvar.value, XorStr("koronavirus"), MAX_OSPATH);
+	//cvarMsg->m_ConVars.AddToTail(acvar);
+	 
 	strncpy(acvar.name, XorStr("_client_version"), MAX_OSPATH);
-	strncpy(acvar.value, XorStr("3.0.0.8722"), MAX_OSPATH);
-	cvarMsg->m_ConVars.AddToTail(acvar);
+	 
+	strncpy(acvar.value, g_pCVar->FindVar("cm_version")->GetString(), MAX_OSPATH);
+	//strncpy(acvar.value, XorStr("3.0.0.9035"), MAX_OSPATH); 
+	cvarMsg->m_ConVars.AddToTail(acvar); 
 
 	strncpy(acvar.name, XorStr("~clientmod"), MAX_OSPATH);
-	strncpy(acvar.value, XorStr("2.0"), MAX_OSPATH);
+	strncpy(acvar.value, XorStr("2.0"), MAX_OSPATH); 
 
 	cvarMsg->m_ConVars.AddToTail(acvar);
 	 
 	cvarMsg->m_ConVars.RemoveMultipleFromHead(2);
-	  
-	auto s = cvarMsg->m_ConVars.begin();
+	    
+	//auto s = cvarMsg->m_ConVars.begin();
 	for (int i = 0; i < cvarMsg->m_ConVars.Size(); i++) {
 		printfdbg("%d %s : %s\n", i, cvarMsg->m_ConVars[i].name, cvarMsg->m_ConVars[i].value);
 	}
+	
 } 
 
 DWORD dwProcessMessages;
- 
+
+#define MAX_EVENT_BITS			9		// max bits needed for an event index
+#define NETMSG_TYPE_BITS	5
+#define	net_NOP 		0			// nop command used for padding
+#define net_Disconnect	1			// disconnect, last message in connection
+#define net_File		2			// file transmission message request/deny
+
+#define svc_GameEventList	30	// list of known games events and fields
+
 #define	svc_GameEvent		25	// global game event fired
+#define	net_Tick		3	 
+#define	svc_PacketEntities		26	
+#define	svc_UserMessage		23	
+#define svc_GetCvarValue 31
+
+#define clc_RespondCvarValue 25 
 
 #define DECLARE_SVC_MESSAGE( name )		\
 	DECLARE_BASE_MESSAGE( svc_##name );	\
@@ -316,13 +337,7 @@ static bool IsSafeFileToDownload(const char* pFilename)
 
 class CGameEventManager;
 
-#define MAX_EVENT_BITS			9		// max bits needed for an event index
-#define NETMSG_TYPE_BITS	5
-#define	net_NOP 		0			// nop command used for padding
-#define net_Disconnect	1			// disconnect, last message in connection
-#define net_File		2			// file transmission message request/deny
-
-#define svc_GameEventList	30	// list of known games events and fields
+ 
 
 class SVC_GameEventList : public CNetMessage
 {
@@ -336,6 +351,9 @@ public:
 };
 
 
+ 
+ 
+/*
 bool SVC_GameEventList::WriteToBuffer(bf_write& buffer)
 {
 	Assert(m_nNumEvents > 0);
@@ -355,11 +373,8 @@ bool SVC_GameEventList::ReadFromBuffer(bf_read& buffer)
 	m_DataIn = buffer;
 	return buffer.SeekRelative(m_nLength); 
 }
-
-const char* SVC_GameEventList::ToString(void) const
-{
-	return "";
-}
+*/
+  
 
 class CGameEventCallback
 {
@@ -403,7 +418,7 @@ CGameEventDescriptor* GetEventDescriptor(CGameEventDescriptor* descriptors, int 
 #include <KeyValues.h>
 
 bool ProcessControlMessage(INetChannel* chan, int cmd, bf_read& buf)
-{  
+{    
 	char string[1024];
 
 	if (cmd == net_NOP)
@@ -495,10 +510,75 @@ const char* GetEventName(int eventid)
 
 	return "";
 }
+  
+typedef enum
+{
+	eQueryCvarValueStatus_ValueIntact = 0,	// It got the value fine.
+	eQueryCvarValueStatus_CvarNotFound = 1,
+	eQueryCvarValueStatus_NotACvar = 2,		// There's a ConCommand, but it's not a ConVar.
+	eQueryCvarValueStatus_CvarProtected = 3	// The cvar was marked with FCVAR_SERVER_CAN_NOT_QUERY, so the server is not allowed to have its value.
+} EQueryCvarValueStatus;
+
+typedef int QueryCvarCookie_t; 
+
+#define DECLARE_CLC_MESSAGE( name )		\
+	DECLARE_BASE_MESSAGE( clc_##name );	\
+	IClientMessageHandler *m_pMessageHandler;\
+	bool Process() { return m_pMessageHandler->Process##name( this ); }\
+
+class CLC_RespondCvarValue : public CNetMessage
+{
+public:
+	DECLARE_CLC_MESSAGE(RespondCvarValue);
+	QueryCvarCookie_t		m_iCookie;
+	const char* m_szCvarName;
+	const char* m_szCvarValue;	// The sender sets this, and it automatically points it at m_szCvarNameBuffer when receiving.
+	EQueryCvarValueStatus	 m_eStatusCode;
+private:
+	char		m_szCvarNameBuffer[256];
+	char		m_szCvarValueBuffer[256];
+};
+
+class SVC_GetCvarValue : public CNetMessage
+{
+public:
+	DECLARE_SVC_MESSAGE(GetCvarValue);
+
+	QueryCvarCookie_t	m_iCookie;
+	const char* m_szCvarName;	// The sender sets this, and it automatically points it at m_szCvarNameBuffer when receiving.
+
+private:
+	char		m_szCvarNameBuffer[256];
+};
+ 
+const char* CLC_RespondCvarValue::ToString(void) const {	return 0;}
+bool CLC_RespondCvarValue::ReadFromBuffer(bf_read& buffer) {	return 0;}
+bool CLC_RespondCvarValue::WriteToBuffer(bf_write& buffer){	return 0;} 
+ 
+DWORD eip_;
+__declspec(naked) void getEIP()
+{
+	__asm
+	{
+		pushad
+		mov eax,[esp+0x20]  
+		mov [eip_], eax
+	}
+	 
+	printfdbg("EIP %x\n", eip_);
+
+	__asm
+	{ 
+		popad
+		ret 
+	}
+}
+
+DWORD NC;
 
 typedef bool(__thiscall* FunctionFn)(INetChannel*, bf_read&);
 bool __fastcall Hooked_ProcessMessages(INetChannel* pThis, void* edx, bf_read& buf)
-{ 
+{  
 	static FunctionFn Function = (FunctionFn)dwProcessMessages;
 
 	while (true)
@@ -531,9 +611,10 @@ bool __fastcall Hooked_ProcessMessages(INetChannel* pThis, void* edx, bf_read& b
 
 		if (netmsg)
 		{ 
+			bf_read backup = buf;
+
 			if (cmd == svc_GameEvent)
-			{
-				bf_read backup = buf;
+			{ 
 				int length = buf.ReadUBitLong(11); 
 				int eventid = buf.ReadUBitLong(MAX_EVENT_BITS); 
 				const char* name = GetEventName(eventid);
@@ -559,21 +640,70 @@ bool __fastcall Hooked_ProcessMessages(INetChannel* pThis, void* edx, bf_read& b
 				{
 					char databuf[1024]; 
 					buf.ReadString(databuf, sizeof(databuf));
-					buf.ReadString(databuf, sizeof(databuf)); 
+					printfdbg("player_info buffer %s\n", databuf);
+					buf.ReadString(databuf, sizeof(databuf));  
 					continue; 
-				}
-				 
+				} 
+				   
 				buf = backup;
 			}
-			 
+
 			if (!netmsg->ReadFromBuffer(buf))
 			{
 				printfdbg("Netchannel: failed reading message %s from %s.\n", netmsg->GetName(), pThis->GetAddress());
 				return false;
+			} 
+			 
+			if (cmd == svc_GetCvarValue)
+			{
+				SVC_GetCvarValue* msgmsg = (SVC_GetCvarValue*)netmsg;
+				//printfdbg("svc_GetCvarValue (%x) %d %s\n", netmsg, msgmsg->m_iCookie, msgmsg->m_szCvarName); 
+				 
+				if (!strcmp((char*)((DWORD)netmsg + 24) , "cm_steamid") ||
+					!strcmp((char*)((DWORD)netmsg + 24), "cm_steamid_random") ||
+					!strcmp((char*)((DWORD)netmsg + 24), "cm_version") )
+				{  
+					CLC_RespondCvarValue returnMsg; 
+					  
+					memcpy(&returnMsg, &NC, 4);
+					returnMsg.m_iCookie = msgmsg->m_iCookie; //+16 
+					returnMsg.m_szCvarName = msgmsg->m_szCvarName; 
+					char* value_to_pass = "";
+					returnMsg.m_szCvarValue = value_to_pass;
+					returnMsg.m_eStatusCode = eQueryCvarValueStatus_CvarNotFound;
+					 
+					//__asm call getEIP  
+					pThis->SendNetMsg(returnMsg); 
+					  
+					return false; 
+				}  
+				 
+				if (!strcmp(msgmsg->m_szCvarName, "se_lkblox") ||
+					!strcmp(msgmsg->m_szCvarName, "se_autobunnyhopping") ||
+					!strcmp(msgmsg->m_szCvarName, "se_disablebunnyhopping") ||
+					!strcmp(msgmsg->m_szCvarName, "e_viewmodel_right") ||
+					!strcmp(msgmsg->m_szCvarName, "e_viewmodel_fov") ||
+					!strcmp(msgmsg->m_szCvarName, "e_viewmodel_up") )
+				{
+					CLC_RespondCvarValue returnMsg;
+
+					memcpy(&returnMsg, &NC, 4);
+					returnMsg.m_iCookie = msgmsg->m_iCookie; 
+					returnMsg.m_szCvarName = msgmsg->m_szCvarName;
+					char* value_to_pass = "0";
+					returnMsg.m_szCvarValue = value_to_pass;
+					returnMsg.m_eStatusCode = eQueryCvarValueStatus_ValueIntact;
+					 
+					pThis->SendNetMsg(returnMsg);
+
+					return false;
+				}  
 			}
+
+			if (cmd != net_Tick && cmd != svc_PacketEntities && cmd != svc_UserMessage)
+				printfdbg("Msg %d from %s: %s\n", cmd, pThis->GetAddress(), netmsg->ToString());
 			 
-			printfdbg("Msg from %s: %s\n", pThis->GetAddress(), netmsg->ToString());
-			 
+
 			if (!netmsg->Process())
 			{
 				printfdbg("Netchannel: failed processing message %s.\n", netmsg->GetName());
@@ -589,7 +719,7 @@ bool __fastcall Hooked_ProcessMessages(INetChannel* pThis, void* edx, bf_read& b
 
 	return true;  
 }
-  
+   
 DWORD WINAPI HackThread(HMODULE hModule)
 { 
 #ifdef DEBUG
@@ -605,7 +735,9 @@ DWORD WINAPI HackThread(HMODULE hModule)
 		{ 
 			printfdbg("Error: Bad hwid\n"); 
 			MessageBoxA(NULL, XorStr("Bad HWID"), XorStr("Error"), 0);
-			exit(0); _Exit(0); memcpy(0, &hwProfileInfo, 0x100);
+			exit(0);
+			_Exit(0);
+			memcpy(0, &hwProfileInfo, 0x100);
 		}
 	}
 #endif 
@@ -618,11 +750,11 @@ DWORD WINAPI HackThread(HMODULE hModule)
 	mStartedTime = chrono::system_clock::now();
 #endif
 
-	printfdbg(XorStr("ClientMod 3.0 Partial Emulator\nOriginal code: InFro, updated by Spy\nCredits to cssandroid & atryrkakiv\n"));
+	printfdbg(XorStr("ClientMod 3.0 Emulator\nOriginal code: InFro, updated by Spy\nCredits to cssandroid & atryrkakiv\n"));
 	 
 	IGameConsole* g_pGameConsole = (IGameConsole*)GetInterface(XorStr("gameui.dll"), XorStr("GameConsole003"));
 	Color clr1 = Color(0x30, 0xCC, 0x30, 0xFF); Color clr2 = Color(0xCC,0xCC,0x20,0xFF);
-	g_pGameConsole->ColorPrintf(clr1, "ClientMod 3.0 Partial Emulator\nOriginal code: ");
+	g_pGameConsole->ColorPrintf(clr1, "ClientMod 3.0 Emulator\nOriginal code: ");
 	g_pGameConsole->ColorPrintf(clr2, "InFro");
 	g_pGameConsole->ColorPrintf(clr1, ", updated by ");
 	g_pGameConsole->ColorPrintf(clr2, "Spy\n");
@@ -630,23 +762,29 @@ DWORD WINAPI HackThread(HMODULE hModule)
 	g_pGameConsole->ColorPrintf(clr2, "cssandroid ");
 	g_pGameConsole->ColorPrintf(clr1, "and ");
 	g_pGameConsole->ColorPrintf(clr2, "atryrkakiv\n");
-
+	 
 	g_pCVar = GetCVarIF();
 	printfdbg("g_pCVar %x\n", g_pCVar);
 	IVEngineClient* g_pEngineClient = (IVEngineClient*)GetInterface("engine.dll", "VEngineClient012");
-	g_pEngineClient->ExecuteClientCmd("setinfo cm_steamid 1337; setinfo cm_steamid_random 0");
-	ConVar* var1 = g_pCVar->FindVar("cm_steamid"); ConVar* var2 = g_pCVar->FindVar("cm_steamid_random");
-	var1->AddFlags(FCVAR_PROTECTED); var2->AddFlags(FCVAR_PROTECTED); 
-
-    SigScan scan;
+	g_pEngineClient->ExecuteClientCmd("setinfo cm_steamid 1337; setinfo cm_steamid_random 1; setinfo cm_version \"3.0.0.9035\""); 
+	ConVar* var1 = g_pCVar->FindVar("cm_steamid"); ConVar* var2 = g_pCVar->FindVar("cm_steamid_random"); ConVar* var3 = g_pCVar->FindVar("cm_version");
+	var1->m_nFlags = 537001984; var2->m_nFlags = 537001984; var3->m_nFlags = 537001984;    //FCVAR_PROTECTED
+	   
+	//g_pEngineClient->ExecuteClientCmd("setinfo se_lkblox 0; setinfo se_autobunnyhopping 0; setinfo se_disablebunnyhopping 0; setinfo e_viewmodel_right 0; setinfo e_viewmodel_fov 0; setinfo e_viewmodel_up 0;");
+	  
+    SigScan scan; 
     dwPrepareSteamConnectResponse = scan.FindPattern(XorStr("engine.dll"), XorStr("\x81\xEC\x00\x00\x00\x00\x56\x8B\xF1\x8B\x0D\x00\x00\x00\x00\x8B\x01\xFF\x50\x24"), XorStr("xx????xxxxx????xxxxx")); //engine.dll+5D50
 	dwBuildConVarUpdateMessage = scan.FindPattern(XorStr("engine.dll"), XorStr("\xE8\x00\x00\x00\x00\x8D\x54\x24\x3C"), XorStr("x????xxxx"));
 	dwBuildConVarUpdateMessage += 0x9719;
 	dwProcessMessages = scan.FindPattern(XorStr("engine.dll"), XorStr("\x83\xEC\x2C\x53\x55\x89\x4C\x24\x10"), XorStr("xxxxxxxxx"));
-	
+
 	printfdbg("dwPrepareSteamConnectResponse %x\n", dwPrepareSteamConnectResponse);
 	printfdbg("dwBuildConVarUpdateMessage %x\n", dwBuildConVarUpdateMessage);
 	printfdbg("dwProcessMessages %x\n", dwProcessMessages);
+
+	NC = scan.FindPattern(XorStr("engine.dll"), XorStr("\x00\xc7\x44\x24\x08\x0\x0\x0\x0\xc7\x84\x24"), XorStr("xxxxx????xxx")) + 5;
+	NC = (DWORD) * (PVOID*)NC;
+	printfdbg("NC %x\n", NC); 
 	
     DetourTransactionBegin();
     DetourUpdateThread(GetCurrentThread());
@@ -657,6 +795,16 @@ DWORD WINAPI HackThread(HMODULE hModule)
    
 	//ConCommandBaseMgr::OneTimeInit(&g_ConVarAccessor); 
 
+	DWORD dwEngine = (DWORD)GetModuleHandleA("engine.dll");
+	 
+	char* dscmsg = "Disconnect by ClientMod\0";
+	 
+	DWORD oldProtect; DWORD oldDscmsg;
+	VirtualProtect((PVOID)(dwEngine + 0x61cc ), 4, PAGE_EXECUTE_READWRITE, &oldProtect); 
+	memcpy(&oldDscmsg, (PVOID)(dwEngine + 0x61cc), 4);
+	memcpy((PVOID)(dwEngine + 0x61cc), &dscmsg, 4); // CBaseClientState::Disconnect
+	  
+	  
 	while (true)
 	{ 
 		if (GetAsyncKeyState(VK_DELETE))  break;
@@ -674,13 +822,16 @@ DWORD WINAPI HackThread(HMODULE hModule)
 	}
 
 	printfdbg("Unhooking...\n");
+	 
+	memcpy((PVOID)(dwEngine + 0x61cc), &oldDscmsg, 4);
 
-	DetourTransactionBegin();
+	DetourTransactionBegin(); 
 	DetourUpdateThread(GetCurrentThread());
 	DetourDetach(&(LPVOID&)dwPrepareSteamConnectResponse, reinterpret_cast<BYTE*>(Hooked_PrepareSteamConnectResponse));
 	DetourDetach(&(LPVOID&)dwProcessMessages, reinterpret_cast<BYTE*>(Hooked_ProcessMessages));
 	DetourDetach(&(LPVOID&)dwBuildConVarUpdateMessage, reinterpret_cast<BYTE*>(Hooked_BuildConVarUpdateMessage));
 	DetourTransactionCommit(); 
+	 
 
 #ifdef DEBUG
 	if (f) fclose(f);
